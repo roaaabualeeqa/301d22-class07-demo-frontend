25c97e5209f0ca0a47b65a039956ed78b5cf8900
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2021-06-14 07:43:05 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokeInfo:{}
+    }
+  }
+
+
+  getPokemonData = async () =>{
+    let serverRoute = process.env.REACT_APP_SERVER;
+    // http://localhost:3001/test
+    // const url = `${serverRoute}/test`;
+
+    // http://localhost:3001/getPoke?pokeName=bulbasaur
+    const url = `${serverRoute}/getPoke?pokeName=bulbasaur`;
+    // console.log(url);
+    const pokemonItem = await axios.get(url);
+    console.log(pokemonItem.data);
+    this.setState({
+      pokeInfo:pokemonItem.data
+    })
+
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Front end website</h1>
+        <button onClick={this.getPokemonData}> Get Poke data</button>
+        <p>{this.state.pokeInfo.name}</p>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
